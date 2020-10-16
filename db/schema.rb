0e4353e40aadfd5005de39a260e553c54cf5a7ef
Insight-Creator <luizbuchhalter@Outlook.com>
2020-10-16 16:28:14 -0300

    MVC - Back and Front end.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_10_16_191737) do
+
+  create_table "flats", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "address"
+    t.integer "price_per_night"
+    t.integer "number_of_guests"
+    t.string "picture_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
